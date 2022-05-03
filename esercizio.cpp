ee63493dfa5cc50a37d6b4af882684f5b3306889
fafio04 <100995688+fafio04@users.noreply.github.com>
2022-05-03 18:49:14 +0200

    Add files via upload

--- esercizio.cpp
+++ esercizio.cpp
@@ -7,6 +7,10 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <iostream>
+
+using namespace std;
+
+#include <iostream>
 #define DIM 50
 using namespace std;
 
@@ -63,13 +67,72 @@ void stampa(Libri bibblioteca[], int n)
     }
 }
 
+void elimina(Libri bibblioteca[], int n)
+{
+    string elimina;
+    int i = 0;
+    
+    cout<<"inserire il codice dle libro che si vuol eliminare"<<endl;
+    cin>>elimina;
+    
+    while(bibblioteca[i].codice != elimina)
+    {
+        for(i = 0 ; i < n; i++)
+        {
+            bibblioteca[i].titolo = "";
+            bibblioteca[i].autore = "";
+            bibblioteca[i].editore = "";
+            bibblioteca[i].anno = 0;
+            bibblioteca[i].prezzo = 0;
+            bibblioteca[i].codice = "";
+            bibblioteca[i].genere = "";
+        }
+    }
+}
+
+void menu(Libri bibblioteca[], int n, int s)
+{
+    do
+  {
+      cout<<"1 inserimento libri \n| 2 stampa libri \n| 3 elimina libro \n| 4 cercare un libro \n| 5 cambiare libro \n|6 uscita menù|"<<endl;
+      cin>>s;
+    switch(s){
+    
+    case 1:
+    
+        n = inserimento(bibblioteca);
+    break;
+    case 2:
+    
+       stampa(bibblioteca, n); 
+    break;
+    case 3:
+    
+        elimina(bibblioteca, n);
+    break;
+    case 4:
+        
+    break;
+    case 5:
+        
+    break;
+    case 6:
+        cout<<"uscita menù"<<endl;
+        
+    default:
+    cout << "Uscita dal menù" << endl;
+    }
+  }while(s<=5);
+  
+}
+
 int main()
 {
     Libri bibblioteca[DIM];
     int n = 0;
+    int s = 0;
     
-    n = inserimento(bibblioteca);
-    stampa(bibblioteca, n);
+    menu(bibblioteca, n, s);
     
     return 0;
 }
